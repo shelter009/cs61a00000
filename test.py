5c9d18479feb76a5fbc572bdd01127bc0bed08c4
minesyw <2138245054@qq.com>
2023-03-05 16:40:48 +0800

    complete

--- test.py
+++ test.py
@@ -1,11 +1,5 @@
-import random
-result = []
-def avanage_score(count):
-
-    if count < 15:
-        return result.append(count) 
-    else:
-        a = random.randint(15,20)
-        return result.append(avanage_score(count - a))
-avanage_score(100)
-print(result)
+a = []
+for num in range(4):
+    if num == 2:
+        break
+print(num)
